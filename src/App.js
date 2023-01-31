6dc3185df57d08c58714a8d67033b7f50865c892
theyashpatel <yashpatel26@gmail.com>
2023-01-31 15:29:24 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import QuizView from "./components/QuizView";
+import ScoreView from "./components/ScoreView";
 
 function App() {
+  const questions = [
+    {
+      question: "Total number of oceans in the World is",
+      answers: [{ text: "7" }, { text: "6" }, { text: "5", isCorrect: true }],
+    },
+    {
+      question: "Who is CEO of Tesla?",
+      answers: [
+        { text: "Jeff Bezos" },
+        { text: "Elon Musk", isCorrect: true },
+        { text: "Bill Gates" },
+        { text: "Tony Stark" },
+      ],
+    },
+    {
+      question: "Where is Statue of Liberty is located?",
+      answers: [
+        { text: "India" },
+        { text: "Russia" },
+        { text: "UK" },
+        { text: "USA", isCorrect: true },
+      ],
+    },
+    {
+      question: "What is the capital of Germany?",
+      answers: [
+        { text: "Paris" },
+        { text: "Berlin", isCorrect: true },
+        { text: "London" },
+        { text: "Dublin" },
+      ],
+    },
+    {
+      question: "Which one is the largest tropical rain forest in the world?",
+      answers: [
+        { text: "Amazon", isCorrect: true },
+        { text: "Bosawas" },
+        { text: "Southeast Asian Rain Forest" },
+        { text: "Daintree Rain Forest" },
+      ],
+    },
+  ];
+
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [isQuizOver, setIsQuizOver] = useState(false);
+  const [score, setScore] = useState(0);
+
+  const handleAnswerClick = (isCorrect) => {
+    // check score
+    if (isCorrect) setScore(score + 1);
+
+    const next = currentQuestion + 1;
+    if (next < questions.length) setCurrentQuestion(next);
+    else setIsQuizOver(true);
+  };
+
+  const handleResetClick = () => {
+    setCurrentQuestion(0);
+    setIsQuizOver(false);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isQuizOver ? (
+        <ScoreView handleResetClick={handleResetClick} score={score} />
+      ) : (
+        <QuizView
+          questions={questions}
+          currentQuestion={currentQuestion}
+          handleAnswerClick={handleAnswerClick}
+        />
+      )}
     </div>
   );
 }
